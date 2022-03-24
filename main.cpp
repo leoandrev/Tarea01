abc818a99eb5972ca378bfe24a431c61c7001fec
Leonardo <leonardo.vasquez@utec.edu.pe>
2022-03-23 23:16:51 -0500

    Mensaje: Ramas juntadas

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main(){
     cout << "Hello World!\n";
     cout << "Cambio hecho en rama develop.\n";
+    cout << "Cambio juntado exitosamente en rama main.\n";
     return 0;
 
 }
